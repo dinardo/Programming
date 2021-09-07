0613261f80d0796e180219d238e567278176a997
dinardo <mauro.dinardo@gmail.com>
2021-09-07 22:47:00 +0200

    Minor

--- C++/Serializer/SocketUtils.h
+++ C++/Serializer/SocketUtils.h
@@ -6,7 +6,7 @@
 namespace SocketUtils
 {
   const std::string ENDOFMSG = "ENDOFMSG";
-  const int SLEEPTIME = 5; // [s]
+  const int SLEEPTIME = 5; // [ms]
 
   std::string getData(boost::asio::ip::tcp::socket& socket, const std::string& endOfMsg);
   void sendData(boost::asio::ip::tcp::socket& socket, const std::string& message, const std::string& endOfMsg);
