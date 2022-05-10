eb966d2bb93d47f28202cfb7614f22d4ef023e08
Mauro <mauro.dinardo@gmail.com>
2022-05-10 09:02:40 +0200

    Update Messages.cpp
    More coherent type names

--- C++/SimpleDecorator/Messages.cpp
+++ C++/SimpleDecorator/Messages.cpp
@@ -39,12 +39,12 @@ public:
   virtual void Start(int runNumbe, float parameter) = 0;
   virtual int  Stop()                               = 0;
 
-  template<typename M, typename funType, typename classType, typename... Args>
-  void MsgDecorator(M* msg, funType func, classType& obj, Args... args)
+  template<typename M, typename FunType, typename ObjType, typename... Args>
+  void MsgDecorator(M& msg, FunType fun, ObjType& obj, Args... args)
   {
     std::cout << __PRETTY_FUNCTION__ << std::endl;
-    msg->output = "DEFAULT";
-    msg->val    = -10;
+    msg.output = "DEFAULT";
+    msg.val    = -10;
   }
 };
 
@@ -68,40 +68,40 @@ public:
     return 30;
   }
 
-  template<typename M, typename funType, typename classType, typename... Args>
-  void MsgDecorator(M msg, funType func, classType& obj, Args... args)
+  template<typename M, typename FunType, typename ObjType, typename... Args>
+  void MsgDecorator(M& msg, FunType fun, ObjType& obj, Args... args)
   {
-    std::cout << "Function type: " << typeid(func).name() << " " << typeid(funType).name() << std::endl;
+    std::cout << "Function type: " << typeid(fun).name() << " " << typeid(FunType).name() << std::endl;
 
-    if (std::is_same<funType, decltype(&classType::Start)>::value == true)
+    if (std::is_same<FunType, decltype(&ObjType::Start)>::value == true)
       {
         try
           {
-            (obj.*func)(args...);
-            msg->val    = internal;
-            msg->output = "GOOD";
+            (obj.*fun)(args...);
+            msg.val    = internal;
+            msg.output = "GOOD";
           }
         catch(...)
           {
-            msg->val    = 0;
-            msg->output = "BAD";
+            msg.val    = 0;
+            msg.output = "BAD";
           }
       }
-    else if (std::is_same<funType, decltype(&classType::Stop)>::value == true)
+    else if (std::is_same<FunType, decltype(&ObjType::Stop)>::value == true)
       {
         std::cout << "SPECIAL CASE" << std::endl;
 
         try
           {
-            // msg->defaultVal = (obj.*func)(args...); // Not working
-            (obj.*func)(args...);
-            msg->defaultVal = 1;
-            msg->output     = "GOOD STOP";
+            // msg.defaultVal = (obj.*fun)(args...); // Not working
+            (obj.*fun)(args...);
+            msg.defaultVal = 1;
+            msg.output     = "STOP: GOOD";
           }
         catch(...)
           {
-            msg->defaultVal = 0;
-            msg->output     = "BAD STOP";
+            msg.defaultVal = 0;
+            msg.output     = "STOP: BAD";
           }
       }
   }
@@ -137,25 +137,25 @@ int main()
 {
   int   runNumber = 1;
   float parameter = 0;
-  MyMessage* msg  = new MyMessage;
+  MyMessage msg;;
 
 
   PixelAlive pa;
   std::cout << "PixelAlive" << std::endl;
   pa.MsgDecorator(msg, &PixelAlive::Start, pa, runNumber, parameter);
-  std::cout << msg->output << " " << msg->val << " " << msg->defaultVal << std::endl;
+  std::cout << msg.output << " " << msg.val << " " << msg.defaultVal << std::endl;
 
   pa.MsgDecorator(msg, &PixelAlive::Stop, pa);
-  std::cout << msg->output << " " << msg->val << " " << msg->defaultVal << std::endl;
+  std::cout << msg.output << " " << msg.val << " " << msg.defaultVal << std::endl;
 
 
   PedeNoise pn;
   std::cout << "\nPedeNoise" << std::endl;
   pn.MsgDecorator(msg, &PedeNoise::Start, pn, runNumber, parameter);
-  std::cout << msg->output << " " << msg->val << " " << msg->defaultVal << std::endl;
+  std::cout << msg.output << " " << msg.val << " " << msg.defaultVal << std::endl;
 
   pn.MsgDecorator(msg, &PedeNoise::Stop, pn);
-  std::cout << msg->output << " " << msg->val << " " << msg->defaultVal << std::endl;
+  std::cout << msg.output << " " << msg.val << " " << msg.defaultVal << std::endl;
 
 
   return 0;
