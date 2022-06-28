7a1f525982c6ff2b457bb4560b3f9ea56dc1cb52
Mauro <mauro.dinardo@gmail.com>
2022-06-28 21:52:51 +0200

    Update Commands.h

--- C++/VariadicTemplates/Commands.h
+++ C++/VariadicTemplates/Commands.h
@@ -60,30 +60,11 @@ namespace Commands
   protected:
     template <int... Sizes, class... Args>
     uint8_t packAndEncode(Args&&... args) const { return Commands::map5to8bit[bitWise::pack<Sizes...>(std::forward<Args>(args)...)]; }
-    // In general && allows to use a reference or a temporary
-    // In this specific case of template it is good practice because we don't know what we are going to get from the parameter pack
-    // std::forward either copy or move, depending on the whether we have variables or temporarires
-    // Perfect forwarding: instead of these two classes, we can use std::forward
-
-    // template <typename T, typename Arg>
-    // T create(Arg& a) { return T(a); }
-
-    // template <typename T, typename Arg>
-    // T create(const Arg& a) { return T(a); }
-
-    // template <typename T, typename Arg>
-    // T create(Arg&& a) { return T(std::forward<Arg>(a)); }
-
-    // int main()
-    // {
-    //   //L-value
-    //   int five    = 5;
-    //   int myFive1 = create<int>(five);
-    //   std::cout << "myFive1: "  << myFive1 << std::endl;
-    //   // R-value
-    //   int myFive2 = create<int>(5);
-    //   std::cout << "myFive2: " << myFive2 << std::endl;
-    // }
+    // In general &&, called R-value reference or universal reference, allows using a reference or a temporary
+    // In this specific case of template it is good practice because we don't know what we are going to get
+    // from the parameter pack
+    // std::forward, together with the universal reference, is called perfect forwarding and
+    // it either copies or moves, depending on whether we have variables or temporaries
   };
 
   struct WriteCmd : public ComposeCommand<Commands::Write, 4>
