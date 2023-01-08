c0fdb971fbb6a3544a695f466b241d746afa401f
dinardo <mauro.dinardo@gmail.com>
2023-01-08 22:34:33 +0100

    Implemented exception handling

--- C++/Threads/myThreads.cc
+++ C++/Threads/myThreads.cc
@@ -9,12 +9,18 @@ clang++  -ansi -pedantic -Wall -Wextra -m64 -O3 --std=c++11 -o myThreads myThrea
 #include <thread>
 #include <future>
 #include <mutex>
+#include <condition_variable>
 
+
+// ###################################
+// # Producer / Consumer interaction #
+// ###################################
 
 std::mutex myMutex;
-static std::deque< std::pair<int,int> > sharedMemory;
+static std::deque< std::pair<int, int> > sharedMemory;
 const int endOfProcess = -1;
 const unsigned int microSec = 1000000;
+
 
 typedef struct
 {
@@ -22,18 +28,14 @@ typedef struct
   unsigned int howMany;
 }  producerData;
 
+
 typedef struct
 {
   int myID;
 } consumerData;
 
-void ThrowException()
-{
-  throw "Testig failure mode";
-}
 
-
-void Producer (producerData myArgs, std::promise<void>& prom)
+void Producer (producerData myArgs)
 {
   std::default_random_engine gen;
   std::uniform_int_distribution<int> niform(0.5*microSec,1.5*microSec);
@@ -57,19 +59,6 @@ void Producer (producerData myArgs, std::promise<void>& prom)
   myMutex.unlock();
 
   std::cout << "Producer " << myArgs.myID << " is done" << std::endl;
-
-  // ########################
-  // # Testing failure mode #
-  // ########################
-  try
-    {
-      ThrowException();
-      prom.set_value();
-    }
-  catch (...)
-    {
-      prom.set_exception(std::current_exception());
-    }
 }
 
 
@@ -104,10 +93,98 @@ void Consumer (consumerData myArgs)
 }
 
 
+// #######################
+// # Handle Start / Stop #
+// #######################
+
+bool                        doExit;
+std::atomic<bool>           keepRunning;
+std::condition_variable_any wakeUp;
+std::recursive_mutex        theMtx;
+
+
+void waitForCompletion()
+{
+  std::unique_lock<std::recursive_mutex> theGuard(theMtx);
+  wakeUp.wait(theGuard, []() { return doExit; });
+}
+
+
+void informImDone()
+{
+    std::unique_lock<std::recursive_mutex> theGuard(theMtx);
+    doExit = true;
+    theGuard.unlock();
+    wakeUp.notify_one();
+}
+
+
+void Start(float& parameter)
+{
+  std::cout << "Start: " << parameter << std::endl;
+
+  // ########################
+  // # Testing failure mode #
+  // ########################
+  while(keepRunning == true)
+    {
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      throw std::runtime_error("Testig failure mode: while Starting");
+    }
+
+  informImDone();
+
+  throw std::runtime_error("Testig failure mode: end of Start");
+}
+
+
+void Stop(std::thread& theThread)
+{
+  keepRunning = false;
+
+  waitForCompletion();
+
+  if(theThread.joinable() == true) theThread.join();
+
+  std::cout << "Stop" << std::endl;
+}
+
+
+void wrapperStart(float& parameter, std::promise<void>&& promise)
+{
+  try
+    {
+      Start(parameter);
+      promise.set_value();
+    }
+  catch (...)
+    {
+      informImDone();
+      promise.set_exception(std::current_exception());
+    }
+}
+
+
 int main ()
 {
-  std::promise<void> prom1;
-  std::promise<void> prom2;
+  // #####################
+  // # Test Start / Stop #
+  // #####################
+
+  std::promise<void> promise;
+  std::future<void> future = promise.get_future();
+  float par = 0;
+
+  keepRunning = true;
+  doExit      = false;
+
+  std::thread thrStart(wrapperStart, std::ref(par), std::move(promise));
+  // thrStart.detach();
+
+
+  // ##########################################
+  // # Test Producer / Consumer through queue #
+  // ##########################################
 
   unsigned int howMany = 10;
   producerData pData;
@@ -115,44 +192,17 @@ int main ()
 
   pData.myID    = 1;
   pData.howMany = howMany;
-  std::thread thrProducer1(Producer, pData, std::ref(prom1));
+  std::thread thrProducer1(Producer, pData);
 
   cData.myID = 1;
   std::thread thrConsumer1(Consumer, cData);
 
   pData.myID    = 2;
   pData.howMany = howMany;
-  std::thread thrProducer2(Producer, pData, std::ref(prom2));
+  std::thread thrProducer2(Producer, pData);
 
   cData.myID = 2;
   std::thread thrConsumer2(Consumer, cData);
-
-
-  // ########################
-  // # Testing failure mode #
-  // ########################
-  std::future<void> fu1 = prom1.get_future();
-  try
-    {
-      fu1.get();
-      std::cout << "Completed successfully\n";
-    }
-  catch (const char *msg)
-    {
-      std::cerr << "Exception: " << msg << "\n";
-    }
-
-  std::future<void> fu2 = prom2.get_future();
-  try
-    {
-      fu2.get();
-      std::cout << "Completed successfully\n";
-    }
-  catch (const char *msg)
-    {
-      std::cerr << "Exception: " << msg << "\n";
-    }
-
 
   thrProducer1.join();
   thrProducer2.join();
@@ -161,5 +211,16 @@ int main ()
   thrConsumer2.join();
 
 
-  // Th.detach();
+  // #####################
+  // # Test Start / Stop #
+  // #####################
+  Stop(thrStart);
+  try
+    {
+      future.get();
+    }
+  catch(std::runtime_error& e)
+    {
+      std::cout << e.what() << std::endl;;
+    }
 }
